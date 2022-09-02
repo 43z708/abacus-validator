d418df57261620f72ec82ee2f44821773f1cfc54
43z708 <s7080303r@gmail.com>
2022-09-03 01:04:10 +0900

    Fix shell

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+if [ ! -e .exec-once ]; then
+touch .exec-once
+chains=(arbitrum avalanche bsc celo ethereum optimism polygon)
+
+for chain in ${chains[@]}; do
+cp ./chain/$chain/.env.sample ./chain/$chain/.env
+done
+
+fi
 
 keys=(AwsAccessKeyId AwsSecretAccessKey KmsKeyId ValidatorName AbcValidatorCheckpointsyncerBucket AbcValidatorRegion AbcValidatorCheckpointsyncerRegion AbcBaseValidatorRegion)
 
